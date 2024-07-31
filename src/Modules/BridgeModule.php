6a59a330e4207f84c150fa1523b7836ddc78f35d
AnasAdham <64723624+AnasAdham@users.noreply.github.com>
2024-07-31 15:19:05 +0800

    Create BridgeModule.php
    Add BridgeModule.php to connect with ala carte and loyalty

---
+++ src/Modules/BridgeModule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RevenueMonster\SDK\Modules;
+// vendor/revenuemonster/sdk/src/RevenueMonster.php
+
+class BridgeModule extends Module
+{
+    public function AlaCartAPI($method, $store)
+    {
+        $uri = $this->getOpenApiUrl('v3', '/service');
+        return $this->mapResponse($this->callApi($method, $uri, $store)->send());
+    }
+
+    public function LoyaltyVoucherAPI($method, $store, $link)
+    {
+        $uri = $this->getOpenApiUrl('v3', '/loyalty'.$link);
+        return $this->mapResponse($this->callApi($method, $uri, $store)->send());
+    }
+}
